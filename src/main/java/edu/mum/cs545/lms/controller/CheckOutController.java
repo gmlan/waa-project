b52f594a68c0333ea8fd57722128d74f7d81702f
mikeafewerki <mikeafewerki@yahoo.com>
2016-05-17 10:36:28 -0500

    checkout controller

--- src/main/java/edu/mum/cs545/lms/controller/CheckOutController.java
+++ src/main/java/edu/mum/cs545/lms/controller/CheckOutController.java
@@ -5,10 +5,50 @@
  */
 package edu.mum.cs545.lms.controller;
 
+import edu.mum.cs545.lms.domain.CheckOutRecord;
+import edu.mum.cs545.lms.domain.User;
+import edu.mum.cs545.lms.service.UserService;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 /**
  *
  * @author Mike
  */
+@Controller
 public class CheckOutController {
     
+    @Autowired
+    UserService userService;
+    
+    @RequestMapping("/checkout")
+    public String checkout(Model model){
+        //model.addAttribute("userlist",userService.getAll());
+        User user1 = new User();
+        user1.setUserId("1122");
+        user1.setFirstName("Michael");
+        
+        User user2 = new User();
+        user2.setUserId("3344");
+        user2.setFirstName("Biniam");
+        
+        List<User> userlist = new ArrayList<User>();
+        userlist.add(user1);
+        userlist.add(user2);
+        
+        model.addAttribute("userlist",userlist);
+        return "checkout";
+    }
+    
+    @RequestMapping(value = "/checkout", method = RequestMethod.POST)
+    public String addNewCheckout(CheckOutRecord checkOutRecord, RedirectAttributes redirectAttributes, Model model){
+        redirectAttributes.addAttribute("tagline","checkout saved");
+        return "redirect:/listCheckout";
+    }
 }
--- src/main/java/edu/mum/cs545/lms/domain/CheckOutRecord.java
+++ src/main/java/edu/mum/cs545/lms/domain/CheckOutRecord.java
@@ -2,63 +2,91 @@ package edu.mum.cs545.lms.domain;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import javax.persistence.CascadeType;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
+@Table(name = "t_checkout")
 public class CheckOutRecord implements Serializable {
-	
-	@Id @GeneratedValue
-	private Long id;
-	private String ISBN;
-	private String memberID;
-	private LocalDate dueDate;
-	private LocalDate dateCheckout;
-	
-	public CheckOutRecord(){}
-	
-	
-	public Long getId() {
-		return id;
-	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String ISBN;
+    private String userId;
+    @Temporal(TemporalType.DATE)
+    private LocalDate dueDate;
+    @Temporal(TemporalType.DATE)
+    private LocalDate dateCheckout;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private User user;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Book book;
 
+    public CheckOutRecord() {
+    }
 
-	public String getMemberID() {
-		return memberID;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setMemberID(String memberID) {
-		this.memberID = memberID;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public LocalDate getDueDate() {
-		return dueDate;
-	}
+    public String getUserId() {
+        return userId;
+    }
 
-	public void setDueDate(LocalDate dueDate) {
-		this.dueDate = dueDate;
-	}
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 
-	public LocalDate getDateCheckout() {
-		return dateCheckout;
-	}
+    public Book getBook() {
+        return book;
+    }
 
-	public void setDateCheckout(LocalDate dateCheckout) {
-		this.dateCheckout = dateCheckout;
-	}
-	
-	public String getISBN() {
-		return ISBN;
-	}
+    public void setBook(Book book) {
+        this.book = book;
+    }
 
-	public void setISBN(String iSBN) {
-		ISBN = iSBN;
-	}
-}	
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
 
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public LocalDate getDateCheckout() {
+        return dateCheckout;
+    }
+
+    public void setDateCheckout(LocalDate dateCheckout) {
+        this.dateCheckout = dateCheckout;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String iSBN) {
+        ISBN = iSBN;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
